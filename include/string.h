9557d380d0e8f9abe17836401b7cc26b7da971c2
jianyuelin <jianyuelin@westwin.com>
2020-12-25 18:36:44 +0800

    new str lib

---
+++ include/string.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdbool.h>
+
+// source: xx=ss
+void get_arg(const char *source, char *arg);
+
+int str_len(const char *str);
+
+int str_index(char source[], char searchfor[]);
