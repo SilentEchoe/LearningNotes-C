59c703e9a744c01069aa05d382aac4c5735ff28f
kai_wang <871779972@qq.com>
2020-12-31 16:39:10 +0800

    打印

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ const char pattern[] = "ould";
 
 int main(int argc, char *argv[]) {
 
-    int a[10] = {0, 1, 2, 3, 4,5,6,7,8,9};
+    int a[10] = {0,1,2,3,4,5,6,7,8,9};
     int  *pa;
     pa = &a[0];
 //    new_print_int_attr(&a[2]);
--- src/printInt.c
+++ src/printInt.c
@@ -8,9 +8,11 @@
 #define GET_ARRAY_LEN(array,len){len = (sizeof(array) / sizeof(array[0]));}
 
 int print_int_attr(int arr[]){
-    int len;
-    GET_ARRAY_LEN(arr,len);
-    for (int i = 0; i < len; ++i) {
+    int len = sizeof (arr) / sizeof (arr[0]);;
+    //GET_ARRAY_LEN(arr,len);
+    printf("%d /n",len);
+
+    for (int i = 0; i < 10; ++i) {
         printf("%d",arr[i]);
     }
 }
