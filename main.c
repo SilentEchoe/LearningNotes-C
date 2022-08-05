cb4430d512d58b591bd803236573a42e5f89ba6d
kai <871779972@qq.com>
2022-08-05 09:56:04 +0800

    struct le

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
+
+struct fraction {
+    int numerator;
+    int denominator;
+};
+
+int main(void) {
+    char s[] = "Goats!";
+    char t[100];
+    memcpy(t, s, sizeof(s));  // 拷贝7个字节，包括终止符
+    printf("%s\n", t);  // "Goats!"
+
+    struct fraction f1;
+    f1.denominator = 22;
+    f1.numerator = 7;
+
     return 0;
 }
+
+
+
+
