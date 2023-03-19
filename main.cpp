a800e26b0f99316d2569bc5fc3ea7fd05f100381
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-03-19 22:35:38 +0800

    if else

--- main.cpp
+++ main.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 #include "Log.h"
 
-void Log(const char* messages){
-    std::cout << messages <<  std::endl;
+void Log(const char *messages)
+{
+    std::cout << messages << std::endl;
 }
 
-int main() {
-    Log("test");
+int main()
+{
+
+    const char *prt = nullptr;
+    if (prt == nullptr)
+    {
+        Log("prt is null");
+    }
+    else
+    {
+        Log("prt not is null");
+    };
     return 0;
 };
