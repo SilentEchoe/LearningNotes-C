3aec1c60a62d1fc13e6ed9e239a34b7b6e4af7cd
anonymous <33675262+AnAnonymousFriend@users.noreply.github.com>
2023-04-16 17:13:31 +0800

    引用

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
-#include "Log.h"
-
-void Log(const char *messages)
-{·
-    std::cout << messages << std::endl;
-}
+#define LOG(x) std::cout << x << std::endl
 
 int main()
 {
-    // 创建一个空指针
 
     int var = 8;
-    void* ptr_nil = &var;
- 
+    int *ptr = &var;
+    *ptr = 10;
 
+    // 创建引用ref 并将其绑定到变量a, 从此ref就是a的别名, 两者的值是一样的,实际上ref并不存在,它只存在于我们的源码中
+
+    int a = 5;
+    int &ref = a;
+
+    ref = 2;
+
+    LOG(ref);
     return 0;
 };
